a174b34df4a43cdafda75ccb8b6329f659a2ed79
Robert Kirsz <robert.kirsz@gmail.com>
2017-07-16 21:43:32 +0200

    Clean-up

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -5,26 +5,24 @@ import _debounce from 'lodash/debounce'
 
 // Firebase configuration
 const productionConfig = {
-  apiKey: "AIzaSyBbVjhaPDhEuPlsLyUaZkZ6c7_wDPcdEo8",
-  authDomain: "magic-collection-8b310.firebaseapp.com",
-  databaseURL: "https://magic-collection-8b310.firebaseio.com",
-  projectId: "magic-collection-8b310",
-  storageBucket: "magic-collection-8b310.appspot.com",
-  messagingSenderId: "2571027304"
+  apiKey: 'AIzaSyBbVjhaPDhEuPlsLyUaZkZ6c7_wDPcdEo8',
+  authDomain: 'magic-collection-8b310.firebaseapp.com',
+  databaseURL: 'https://magic-collection-8b310.firebaseio.com',
+  projectId: 'magic-collection-8b310',
+  storageBucket: 'magic-collection-8b310.appspot.com',
+  messagingSenderId: '2571027304'
 }
 
 const developmentConfig = {
-  apiKey: "AIzaSyBi1hsBdyJgM9w2UW2pOOC-W9oUwOe7nRc",
-  authDomain: "magic-collection-dev.firebaseapp.com",
-  databaseURL: "https://magic-collection-dev.firebaseio.com",
-  projectId: "magic-collection-dev",
-  storageBucket: "magic-collection-dev.appspot.com",
-  messagingSenderId: "1073001376184"
+  apiKey: 'AIzaSyBi1hsBdyJgM9w2UW2pOOC-W9oUwOe7nRc',
+  authDomain: 'magic-collection-dev.firebaseapp.com',
+  databaseURL: 'https://magic-collection-dev.firebaseio.com',
+  projectId: 'magic-collection-dev',
+  storageBucket: 'magic-collection-dev.appspot.com',
+  messagingSenderId: '1073001376184'
 }
 
-const config = process.env.NODE_ENV === 'production'
-  ? productionConfig
-  : developmentConfig
+const config = process.env.NODE_ENV === 'production' ? productionConfig : developmentConfig
 
 export const app = firebase.initializeApp(config)
 export const auth = firebase.auth()
@@ -41,101 +39,87 @@ const providers = {
 // ---------- GENERIC STUFF ----------
 
 // Generic 'get' function
-export const firebaseGetData = (table, id) => (
-  database
-    .ref(table)
-    .child(id)
-    .once('value')
-    .then(snapshot => {
-      const data = snapshot.val()
-      return data
-        ? ({ success: true, data })
-        : ({ error: 'No data found' })
-    })
-)
+export const firebaseGetData = (table, id) =>
+  database.ref(table).child(id).once('value').then(snapshot => {
+    const data = snapshot.val()
+    return data ? { success: true, data } : { error: 'No data found' }
+  })
 
 // Generic 'set' function
-export const firebaseSetData = (table, id, data) => (
+export const firebaseSetData = (table, id, data) =>
   database
     .ref(table)
     .child(id)
     .set(data)
     .then(() => ({ success: true }))
     .catch(response => ({ error: response.message }))
-)
 
 // Generic 'push' function
-export const firebasePushData = (table, data) => (
-  database
-    .ref(table)
-    .push()
-    .set(data)
-    .then(() => ({ success: true }))
-    .catch(response => ({ error: response.message }))
-)
+export const firebasePushData = (table, data) =>
+  database.ref(table).push().set(data).then(() => ({ success: true })).catch(response => ({ error: response.message }))
 
 // Generic 'update' function
-export const firebaseUpdateData = (table, id, data) => (
+export const firebaseUpdateData = (table, id, data) =>
   database
     .ref(table)
     .child(id)
     .update(data)
     .then(() => ({ success: true }))
     .catch(response => ({ error: response.message }))
-)
 
 // ---------- AUTHENTICATION ----------
 
 // Generic email and password sign in
-export const firebaseSignIn = (email, password) => (
-  auth.signInWithEmailAndPassword(email, password)
+export const firebaseSignIn = (email, password) =>
+  auth
+    .signInWithEmailAndPassword(email, password)
     .then(response => ({ success: true, id: response.uid, response }))
     .catch(response => ({ error: response.message, response }))
-)
 
 // Generic email and password sign up
-export const firebaseSignUp = (email, password) => (
-  auth.createUserWithEmailAndPassword(email, password)
+export const firebaseSignUp = (email, password) =>
+  auth
+    .createUserWithEmailAndPassword(email, password)
     .then(response => ({ success: true, id: response.uid }))
     .catch(response => ({ error: response.message }))
-)
 
 // Sign out
-export const firebaseSignOut = () => (
-  auth.signOut()
-    .then(() => ({ success: true }))
-    .catch(response => ({ error: response.message }))
-)
+export const firebaseSignOut = () =>
+  auth.signOut().then(() => ({ success: true })).catch(response => ({ error: response.message }))
 
 // Provider sign in
-export const firebaseProviderSignIn = (providerName) => (
-  auth.signInWithPopup(providers[providerName])
+export const firebaseProviderSignIn = providerName =>
+  auth
+    .signInWithPopup(providers[providerName])
     .then(response => ({ success: true, user: response.user }))
     .catch(response => ({ error: response.message }))
-)
 
 // ---------- PROFILE ----------
 
-export const updateProfile = userProfile => (
-  auth.currentUser.updateProfile({
-    displayName: userProfile.displayName,
-    photoURL: userProfile.photoURL
-  })
-    .then(() => {
-      console.log('Success')
-    }, error => {
-      console.log('Error', error)
+export const updateProfile = userProfile =>
+  auth.currentUser
+    .updateProfile({
+      displayName: userProfile.displayName,
+      photoURL: userProfile.photoURL
     })
-)
+    .then(
+      () => {
+        console.log('Success')
+      },
+      error => {
+        console.log('Error', error)
+      }
+    )
 
-export const updateEmail = email => (
-  auth.currentUser.updateEmail(email)
-    .then(() => {
+export const updateEmail = email =>
+  auth.currentUser.updateEmail(email).then(
+    () => {
       console.log('Success')
-    }, error => {
+    },
+    error => {
       console.log('Error', error)
-    })
-)
+    }
+  )
 
 // ---------- USER DATA UPDATING ----------
 
@@ -149,9 +133,12 @@ export const updateAndReturnUserSettings = settings => {
   return settings
 }
 
+// TODO: this is not used at the moment
 export const saveCollection = collection => {
   const reducedCollection = {}
-  _forEach(collection, singleCard => { reducedCollection[singleCard.id] = singleCard.formatForFirebase() })
+  _forEach(collection, singleCard => {
+    reducedCollection[singleCard.id] = singleCard.formatForFirebase()
+  })
   return firebaseUpdateData('Collections', auth.currentUser.uid, reducedCollection)
 }
 
