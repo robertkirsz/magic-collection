604da5a6180a8c357091f3ea8b0e6c5c41ea3279
Robert Kirsz <robert.kirsz@gmail.com>
2017-07-18 20:31:01 +0200

    Transferred data to the new database

--- src/database/index.js
+++ src/database/index.js
@@ -1,16 +1,13 @@
-export const fetchCards = () => new Promise((resolve, reject) => {
-  const url = process.env.NODE_ENV === 'production'
-    ? 'https://mtgjson.com/json/AllSets.json'
-    : 'http://localhost:3000/AllSets.json'
-
-  fetch(url)
-    .then(response => {
-      if (!response.ok) throw new Error(`${response.status} ${response.statusText}`)
-      return response.json()
-    })
-    .then(data => resolve(data))
-    .catch(error => reject(typeof error === 'string' ? error : error.message))
-})
+export const fetchCards = () =>
+  new Promise((resolve, reject) => {
+    fetch('https://mtgjson.com/json/AllSets.json')
+      .then(response => {
+        if (!response.ok) throw new Error(`${response.status} ${response.statusText}`)
+        return response.json()
+      })
+      .then(data => resolve(data))
+      .catch(error => reject(typeof error === 'string' ? error : error.message))
+  })
 
 let cardsDatabase = []
 
